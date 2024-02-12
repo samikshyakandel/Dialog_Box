0cf326e9497be82adbb426f69cb5eaaeb7fd0455
samikshyakandel <kshasamee8@gmail.com>
2024-02-12 16:40:03 +0545

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/dialog_box/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dialog_box
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/dialog.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class DialogBox extends StatefulWidget {
+  const DialogBox({super.key});
+
+  @override
+  State<DialogBox> createState() => _DialogBoxState();
+}
+
+class _DialogBoxState extends State<DialogBox> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: DialogExample()),
+    );
+  }
+}
+class DialogExample extends StatelessWidget {
+  const DialogExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () => showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('AlertDialog Title'),
+          content: const Text('AlertDialog description'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      ),
+      child: const Text('Show Dialog'),
+    );
+  }
+}
